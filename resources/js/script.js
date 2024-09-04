f0d0d4f854ef8ca9ffbd8479a51d41b33f4dedec
CoderMahabub <CoderMahabub@gmail.com>
2024-09-05 00:41:06 +0900

    Paul

---
+++ resources/js/script.js
@@ -0,0 +1,62 @@
+// SCROLLIFY
+
+$(function(){
+    $.scrollify({
+        section: '.scrollify',
+        setHeights: false,
+        updateHash: false
+    });
+});
+
+// HAMBURGER MENU
+
+function closeMenu() {
+    document.getElementById('navbar').style.height = "0%";
+}
+
+function openMenu() {
+    document.getElementById('navbar').style.height = "100%";
+}
+
+// SMOOTH SCROLL
+
+$(function(){
+    $('.menu-item a').on('click', function(){
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 500);
+        return false;
+    });
+});
+
+
+// STICKY MENU
+
+$(window).on('scroll', function(){
+    if($(window).scrollTop()){
+            $('nav').addClass('sticky');
+       }else {
+            $('nav').removeClass('sticky');
+       }
+});
+
+// COUNTER UP
+
+$('.num').counterUp({
+
+    time: 1000
+});
+
+
+// MIXITUP
+
+var mixer = mixitup('.grid-work-container', {
+    animation: {
+        duration: 200
+    }
+});
+
+
+
+
+
